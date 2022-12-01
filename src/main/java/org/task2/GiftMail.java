dd74430c56b248ca99df8ff470426ce0bc76ab9b
Serhii Matsyshyn <serhii.matsyshyn@ucu.edu.ua>
2022-12-01 15:04:10 +0200

    testing api

--- src/main/java/org/task2/GiftMail.java
+++ src/main/java/org/task2/GiftMail.java
@@ -2,7 +2,7 @@ package org.task2;
 
 public class GiftMail implements MailCode {
     private static String FROM = "!EDUCATIONAL PROJECT! Gift";
-    private static String FromEmail = "gift@example.com";
+    private static String FromEmail = "<example_email>@ucu.edu.ua";
     private static String TEMPLATE = "%NAME congratulations! You've got a gift! !EDUCATIONAL PROJECT!";
     private static String TITLE = "!EDUCATIONAL PROJECT! Congratulations %NAME!";
     public String generate(Client client) {
